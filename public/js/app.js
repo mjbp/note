2837554542f8fd2a35fbcd46e839145563bfc324
mjbp <mickperkins@gmail.com>
2014-12-20 20:21:46 +0000

    Added paste support, various bug fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,14 @@
-/*global $, window, document, console, XMLHttpRequest, Blob, saveAs*/
+/*global $, window, document, navigator, console, XMLHttpRequest, Blob, saveAs*/
 (function (w, d) {
 	'use strict';
-	
-	/*
-	 * LOOK AT MEDIUM EDITOR
-	 * 	- how does it handle 
-	 * 		- key down/up events
-	 *		- enter/p
-	 */
 	
 	var UTILS = {
 			XHR : function (url, content) {
 				var http = new XMLHttpRequest();
 
 				http.onload = function () {
-					if (http.status == 200) {
+					if (http.status === 200) {
+						console.log('Posted...');
 					}
 				};
 				http.open('POST', url, true);
@@ -31,33 +25,10 @@
 			},
 			preventDefault : function (e) {
 				return e.preventDefault ? e.preventDefault() : e.returnValue = false;
-			},/*
-			setCaretPosition : function (target, index) {
-				var selection = w.getSelection(),
-					range = selection.getRangeAt(0);
-
-				range.collapse(true);
-				
-				range.setStart(target.childNodes[0], index);
-				range.collapse(true);
-
-				selection.removeAllRanges();
-				selection.addRange(range);
 			},
-			getCaretPosition : function (source) {
-				var selection = w.getSelection(),
-					range = selection.getRangeAt(0);
-				
-				range.setStart((source.childNodes[0] || source), 0);
-				
-				return range.toString().length;
+			stopPropagation : function (e) {
+				return e.stopPropagation ? e.stopPropagation() : false;
 			},
-			getLastTypedLetter : function () {
-				var selection = w.getSelection(),
-					range = selection.getRangeAt(0);
-				range.setStart(range.startContainer, range.startOffset - 1);
-				console.log(range.toString());
-			},*/
 			getSelectionStartNode : function (parent) {
 				var node = d.getSelection().anchorNode,
 					startNode = (node && node.nodeType === 3 ? node.parentNode : node);
@@ -67,10 +38,52 @@
 				while (node.firstChild) {
 					node.removeChild(node.firstChild);
 				}
+			},
+			htmlEntities: function (str) {
+				// converts special characters (like <) into their escaped/encoded values (like &lt;).
+				// This allows you to show to display the string without the browser reading it as HTML.
+				return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+			},
+			insertHTMLCommand : function(doc, html) {
+				var selection,
+					range,
+					el,
+					fragment,
+					node,
+					lastNode;
+
+				if (doc.queryCommandSupported('insertHTML')) {
+					return doc.execCommand('insertHTML', false, html);
+				}
+
+				selection = window.getSelection();
+				if (selection.getRangeAt && selection.rangeCount) {
+					range = selection.getRangeAt(0);
+					range.deleteContents();
+
+					el = doc.createElement("div");
+					el.innerHTML = html;
+					fragment = doc.createDocumentFragment();
+					while (el.firstChild) {
+						node = el.firstChild;
+						lastNode = fragment.appendChild(node);
+					}
+					range.insertNode(fragment);
+
+					// Preserve the selection:
+					if (lastNode) {
+						range = range.cloneRange();
+						range.setStartAfter(lastNode);
+						range.collapse(true);
+						selection.removeAllRanges();
+						selection.addRange(range);
+					}
+				}
 			}
+
 		};
 	
-	function Note () {
+	function Note() {
 		this.init();
 	}
 
@@ -78,72 +91,22 @@
 		this.setter('storage', w.localStorage)
 			.setter('id', w.location.pathname.substring(w.location.pathname.lastIndexOf("/") + 1))
 			.setter('storeName', 'note-' + this.id)
-			.setter('savedNote', (this.storage.getItem(this.storeName) || ''))
 			.setter('notepad', d.getElementById('note'))
-			.setter('newlineSymbol', '!')
-			//.setter('caretPosition', 0)
+			.setter('savedNote', this.sanitise.decode(this.notepad.innerHTML))
 			.initListeners()
-			.loadNote()
 			.notepad.focus();
 	};
-
+	
 	Note.prototype.initListeners = function () {
 		var self = this;
-		UTILS.on(d, 'keyup', function (e) {self.keyBinder.up.call(self, e);})
-			 .on(d, 'keydown', function (e) {self.keyBinder.down.call(self, e);})
-		     .on(this.notepad, 'click', function () {
-				 //self.caretPosition = UTILS.getCaretPosition(self.notepad);
-				console.log('click...');
-			 })
-			 .on(d.getElementById('btn-email'), 'click', self.mail)
-			 .on(d.getElementById('btn-download'), 'click', self.download);
+		UTILS.on(d, 'keyup', function (e) {self.keyBinder.up.call(self, e); })
+			 .on(d, 'keydown', function (e) {self.keyBinder.down.call(self, e); })
+			 .on(d, 'paste', function (e) {self.pasteHandler.call(self, e); })
+			 .on(d.getElementById('btn-email'), 'click', function (e) { self.mail.call(self, e); })
+			 .on(d.getElementById('btn-download'), 'click', function (e) {self.download.call(self, e); });
 		
 		return this;
 	};
-	
-	/*
-	bindParagraphCreation: function (index) {
-            var self = this;
-            this.on(this.elements[index], 'keypress', function (e) {
-                var node = getSelectionStart.call(self),
-                    tagName;
-                if (e.which === 32) {
-                    tagName = node.tagName.toLowerCase();
-                    if (tagName === 'a') {
-                        document.execCommand('unlink', false, null);
-                    }
-                }
-            });
-
-            this.on(this.elements[index], 'keyup', function (e) {
-                var node = getSelectionStart.call(self),
-                    tagName,
-                    editorElement;
-
-                if (node && node.getAttribute('data-medium-element') && node.children.length === 0 && !(self.options.disableReturn || node.getAttribute('data-disable-return'))) {
-                    document.execCommand('formatBlock', false, 'p');
-                }
-                if (e.which === 13) {
-                    node = getSelectionStart.call(self);
-                    tagName = node.tagName.toLowerCase();
-                    editorElement = self.getSelectionElement();
-
-                    if (!(self.options.disableReturn || editorElement.getAttribute('data-disable-return')) &&
-                        tagName !== 'li' && !self.isListItemChild(node)) {
-                        if (!e.shiftKey) {
-                            document.execCommand('formatBlock', false, 'p');
-                        }
-                        if (tagName === 'a') {
-                            document.execCommand('unlink', false, null);
-                        }
-                    }
-                }
-            });
-            return this;
-        },
-	*/
-	
-	
 	
 	Note.prototype.setter = function (p, v) {
 		this[p] = v;
@@ -160,20 +123,23 @@
 			13 : {
 				key : 'enter',
 				events : ['keyup']
+			},
+			86 : {
+				key : 'paste',
+				events : ['keypress']
 			}/*,
 			37 : 'direction',
 			38 : 'direction',
 			39 : 'direction',
 			40 : 'direction',
-			66 : 'bold',
-			86 : 'paste'*/
+			66 : 'bold''*/
 		},
 		dispatchTable : {
 			enter : function (e) {
 				var self = this,
 					node = UTILS.getSelectionStartNode(),
 					tagName = node.tagName.toLowerCase();
-				console.log('yolo');
+				UTILS.preventDefault(e);
 				if (!e.shiftKey) {
 					d.execCommand('formatBlock', false, 'p');
 				}
@@ -187,7 +153,8 @@
 			}
 			if (this.keyBinder.keyCodes[e.which] && ~this.keyBinder.keyCodes[e.which].events.join(' ').indexOf(e.type)) {
 				UTILS.preventDefault(e);
-				this.keyBinder.dispatchTable[this.keyBinder.keyCodes[e.which].key](e); }
+				this.keyBinder.dispatchTable[this.keyBinder.keyCodes[e.which].key](e);
+			}
 			this.save();
 			
 			return this;
@@ -196,7 +163,6 @@
 			var self = this,
 				node = UTILS.getSelectionStartNode();
 			if (this.keyBinder.keyCodes[e.which] && ~this.keyBinder.keyCodes[e.which].events.join(' ').indexOf(e.type)) {
-				UTILS.preventDefault(e);
 				this.keyBinder.dispatchTable[this.keyBinder.keyCodes[e.which].key](e);
 			}
 		}
@@ -206,222 +172,56 @@
 	Note.prototype.loadNote = function () {
 		this.notepad.innerHTML = this.savedNote;
 		
-		/*
-		var workingText = '',
-			inText = true,
-			brElement = d.createElement('br'),
-			frag = d.createDocumentFragment();
-		
-		UTILS.removeChildren(this.notepad);
-		
-		//iterate over this.noteContent and write to textNode or, if encountering a this.newlineSymbol, add a br element
-		for (var i = 0; i <= this.noteContent.length; i++) {
-			if (this.noteContent[i] !== this.newlineSymbol) {
-				if (!inText) {
-					workingText = '';
-					inText = true;
-				}
-				workingText += this.noteContent[i];
-			} else {
-				if (workingText.length !== 0) {
-					frag.appendChild(d.createTextNode(workingText));
-				}
-				inText = false;
-				frag.appendChild(brElement.cloneNode());
-			}
-		}*/
-		
 		return this;
 	};
 	
+	Note.prototype.pasteHandler = function (e) {
+		var paragraphs,
+			html = '',
+			p,
+			dataFormat = 'text/plain';
+
+		if (w.clipboardData && e.clipboardData === undefined) {//IE
+			e.clipboardData = w.clipboardData;
+			dataFormat= 'Text';
+		}
+		console.log('Pasting...');
+
+		if (e.clipboardData && e.clipboardData.getData) {
+			UTILS.preventDefault(e);
+			paragraphs = e.clipboardData.getData(dataFormat).split(/[\r\n]/g);
+			for (p = 0; p < paragraphs.length; p += 1) {
+				if (paragraphs[p] !== '') {
+					if (navigator.userAgent.match(/firefox/i) && p === 0) {
+						html += UTILS.htmlEntities(paragraphs[p]);
+					} else {
+						html += '<p>' + UTILS.htmlEntities(paragraphs[p]) + '</p>';
+					}
+				}
+			}
+			UTILS.insertHTMLCommand(d, html);
+		}
+	};
 	
-	/*
-	cleanPaste: function (text) {
-            var i, elList, workEl,
-                el = this.getSelectionElement(),
-                multiline = /<p|<br|<div/.test(text),
-                replacements = [
-
-                    // replace two bogus tags that begin pastes from google docs
-                    [new RegExp(/<[^>]*docs-internal-guid[^>]*>/gi), ""],
-                    [new RegExp(/<\/b>(<br[^>]*>)?$/gi), ""],
-
-                     // un-html spaces and newlines inserted by OS X
-                    [new RegExp(/<span class="Apple-converted-space">\s+<\/span>/g), ' '],
-                    [new RegExp(/<br class="Apple-interchange-newline">/g), '<br>'],
-
-                    // replace google docs italics+bold with a span to be replaced once the html is inserted
-                    [new RegExp(/<span[^>]*(font-style:italic;font-weight:bold|font-weight:bold;font-style:italic)[^>]*>/gi), '<span class="replace-with italic bold">'],
-
-                    // replace google docs italics with a span to be replaced once the html is inserted
-                    [new RegExp(/<span[^>]*font-style:italic[^>]*>/gi), '<span class="replace-with italic">'],
-
-                    //[replace google docs bolds with a span to be replaced once the html is inserted
-                    [new RegExp(/<span[^>]*font-weight:bold[^>]*>/gi), '<span class="replace-with bold">'],
-
-                     // replace manually entered b/i/a tags with real ones
-                    [new RegExp(/&lt;(\/?)(i|b|a)&gt;/gi), '<$1$2>'],
-
-                     // replace manually a tags with real ones, converting smart-quotes from google docs
-                    [new RegExp(/&lt;a\s+href=(&quot;|&rdquo;|&ldquo;|“|”)([^&]+)(&quot;|&rdquo;|&ldquo;|“|”)&gt;/gi), '<a href="$2">']
-
-                ];
-
-            for (i = 0; i < replacements.length; i += 1) {
-                text = text.replace(replacements[i][0], replacements[i][1]);
-            }
-
-            if (multiline) {
-
-                // double br's aren't converted to p tags, but we want paragraphs.
-                elList = text.split('<br><br>');
-
-                this.pasteHTML('<p>' + elList.join('</p><p>') + '</p>');
-                this.options.ownerDocument.execCommand('insertText', false, "\n");
-
-                // block element cleanup
-                elList = el.querySelectorAll('a,p,div,br');
-                for (i = 0; i < elList.length; i += 1) {
-
-                    workEl = elList[i];
-
-                    switch (workEl.tagName.toLowerCase()) {
-                    case 'a':
-                        if (this.options.targetBlank){
-                          this.setTargetBlank(workEl);
-                        }
-                        break;
-                    case 'p':
-                    case 'div':
-                        this.filterCommonBlocks(workEl);
-                        break;
-                    case 'br':
-                        this.filterLineBreak(workEl);
-                        break;
-                    }
-
-                }
-
-
-            } else {
-
-                this.pasteHTML(text);
-
-            }
-
-        },
-
-        pasteHTML: function (html) {
-            var elList, workEl, i, fragmentBody, pasteBlock = this.options.ownerDocument.createDocumentFragment();
-
-            pasteBlock.appendChild(this.options.ownerDocument.createElement('body'));
-
-            fragmentBody = pasteBlock.querySelector('body');
-            fragmentBody.innerHTML = html;
-
-            this.cleanupSpans(fragmentBody);
-
-            elList = fragmentBody.querySelectorAll('*');
-            for (i = 0; i < elList.length; i += 1) {
-
-                workEl = elList[i];
-
-                // delete ugly attributes
-                workEl.removeAttribute('class');
-                workEl.removeAttribute('style');
-                workEl.removeAttribute('dir');
-
-                if (workEl.tagName.toLowerCase() === 'meta') {
-                    workEl.parentNode.removeChild(workEl);
-                }
-
-            }
-            this.options.ownerDocument.execCommand('insertHTML', false, fragmentBody.innerHTML.replace(/&nbsp;/g, ' '));
-        },
-        isCommonBlock: function (el) {
-            return (el && (el.tagName.toLowerCase() === 'p' || el.tagName.toLowerCase() === 'div'));
-        },
-        filterCommonBlocks: function (el) {
-            if (/^\s*$/.test(el.textContent)) {
-                el.parentNode.removeChild(el);
-            }
-        },
-        filterLineBreak: function (el) {
-            if (this.isCommonBlock(el.previousElementSibling)) {
-
-                // remove stray br's following common block elements
-                el.parentNode.removeChild(el);
-
-            } else if (this.isCommonBlock(el.parentNode) && (el.parentNode.firstChild === el || el.parentNode.lastChild === el)) {
-
-                // remove br's just inside open or close tags of a div/p
-                el.parentNode.removeChild(el);
-
-            } else if (el.parentNode.childElementCount === 1) {
-
-                // and br's that are the only child of a div/p
-                this.removeWithParent(el);
-
-            }
-
-        },
-
-        // remove an element, including its parent, if it is the only element within its parent
-        removeWithParent: function (el) {
-            if (el && el.parentNode) {
-                if (el.parentNode.parentNode && el.parentNode.childElementCount === 1) {
-                    el.parentNode.parentNode.removeChild(el.parentNode);
-                } else {
-                    el.parentNode.removeChild(el.parentNode);
-                }
-            }
-        },
-
-        cleanupSpans: function (container_el) {
-
-            var i,
-                el,
-                new_el,
-                spans = container_el.querySelectorAll('.replace-with');
-
-            for (i = 0; i < spans.length; i += 1) {
-
-                el = spans[i];
-                new_el = this.options.ownerDocument.createElement(el.classList.contains('bold') ? 'b' : 'i');
-
-                if (el.classList.contains('bold') && el.classList.contains('italic')) {
-
-                    // add an i tag as well if this has both italics and bold
-                    new_el.innerHTML = '<i>' + el.innerHTML + '</i>';
-
-                } else {
-
-                    new_el.innerHTML = el.innerHTML;
-
-                }
-                el.parentNode.replaceChild(new_el, el);
-
-            }
-
-            spans = container_el.querySelectorAll('span');
-            for (i = 0; i < spans.length; i += 1) {
-
-                el = spans[i];
-
-                // remove empty spans, replace others with their contents
-                if (/^\s*$/.test()) {
-                    el.parentNode.removeChild(el);
-                } else {
-                    el.parentNode.replaceChild(this.options.ownerDocument.createTextNode(el.textContent), el);
-                }
-
-            }
-
-        }
-	*/
-	
-	Note.prototype.mail = function () {
-		this.storage.setItem(this.storeName, this.noteContent.join(''));
+	Note.prototype.mail = function (e) {
+		var self = this,
+			form = d.getElementById('form-email'),
+			field = d.getElementById('form-row-email'),
+			off = function () {
+				d.body.className = d.body.className.split(' app-form-mode').join('');
+			};
+			
+			UTILS.preventDefault(e);
+			e.stopPropagation();
+			
+			if (~d.body.className.indexOf(' app-form-mode')) {
+				UTILS.XHR('/mail', 'content=' + this.extractText(this.sanitise.decode(this.notepad.innerHTML)) + '&form-row-email=' + encodeURI(field.value));
+				off();
+			} else {
+				d.body.className += ' app-form-mode';
+				field.focus();
+				UTILS.on(d, 'click', off);
+			}
 		return this;
 	};
 
@@ -441,7 +241,7 @@
 						flag = false;
 					} else {
 						if (node !== undefined && node.nodeType === 1 && !flag) {
-							text += self.newlineSymbol + extract(node.childNodes);
+							text += '\n' + extract(node.childNodes);
 							flag = true;
 						}
 					}
@@ -454,27 +254,30 @@
 	};
 
 	Note.prototype.save = function () {
-		this.setter('savedNote', this.notepad.innerHTML)
+		this.setter('savedNote', this.sanitise.decode(this.notepad.innerHTML))
 			.storage.setItem(this.storeName, this.savedNote);
+		console.log(this.savedNote);
 		UTILS.XHR('/push' + w.location.pathname, 'content=' + this.savedNote);
 		return this;
 	};
 
 	Note.prototype.download = function () {
-		var blob = new Blob([this.sanitise.all(this.notepad.innerHTML)], {type: "text/plain;charset=utf-8"});
+		var blob = new Blob([this.sanitise.elements(this.sanitise.addnl(this.sanitise.decode(this.notepad.innerHTML)))], {type: "text/plain;charset=utf-8"});
 		saveAs(blob, 'Note-' + this.id + '.txt');
 		return this;
 	};
 
 	Note.prototype.sanitise = {
 		addnl : function (content) {
-			return content.trim().replace(/{{note-break}}/g, '\n');
-		},
-		brnl : function (content) {
-			return content.trim().replace(/<br>/g, '\n');
+			return content.trim().replace(/(<\/p>)/g, '\n');
 		},
 		addbr : function (content) {
 			return content.trim().replace(/{{note-break}}/g, '<br>');
+		},
+		decode : function (content) {
+    		var txtrea = d.createElement("textarea");
+    		txtrea.innerHTML = content;
+			return txtrea.value;
 		},
 		elements : function (content) {
 			return content.replace(/(<([^>]+)>)/ig, '');
